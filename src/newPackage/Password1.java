f10ca9a683216c54d1e3cb4479f9cd3217cf952f
grzehoo <grzehoo.wrzosek@gmail.com>
2020-08-25 12:41:52 +0100

    program completed

--- src/newPackage/Password1.java
+++ src/newPackage/Password1.java
@@ -1,8 +1,32 @@
 package newPackage;
 
+import java.io.Console;
+
 public class Password1 {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String password = "enigma";
+        boolean validPassword;
+
+        Console console = System.console();
+        char[] letters = null;
+
+        int attemptes = 3;
+
+        while (attemptes-- != 0){
+            console.format("Enter password: ");
+            letters = console.readPassword();
+
+            if (String.valueOf(letters).equals(password)){
+                console.format("Correct Password!!!");
+                break;
+            }else {
+                console.format("Incorrect password " + attemptes + " left\n");
+                if (attemptes == 0){
+                    console.printf("Limit attempts was used!!!\nNext try for 15 minutes\n");
+                }
+            }
+        }
     }
 }
